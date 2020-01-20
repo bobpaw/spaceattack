3f7299b52860827ec6f0bacec29412f0cdedd7bc
Aiden Woodruff <aiden.woodruff@gmail.com>
2020-01-19 23:00:49 -0600

    Refuse to use SDL_main
    Remove variable from CMakeSettings.json

--- src/main.cc
+++ src/main.cc
@@ -26,6 +26,7 @@ const int kScreenHeight = 400;
 const int kMaxLives = 10;
 const int kMaxBombs = 5; // Max number of fireable bombs
 
+#undef main
 int main (int argc, char * argv[]) {
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     std::cerr << "SDL couldn't initialize! SDL_ERROR: " << SDL_GetError() << std::endl;
